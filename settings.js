af705d26245d0b2a6f13228bde8383cc58dd1f83
Jhony905 <151104346+Jhony905@users.noreply.github.com>
2025-06-17 11:07:23 -0500

    settings.js

--- settings.js
+++ settings.js
@@ -1,2 +1,19 @@
-flowFile: 'flows_srv-d188t13ipnbc73bahc20-hibernate-9c749677d-2s2kg.json',
-credentialSecret: process.env.CREDENTIAL_SECRET || 'clave-segura',
+//flowFile: 'flows_srv-d188t13ipnbc73bahc20-hibernate-9c749677d-2s2kg.json',
+//credentialSecret: process.env.CREDENTIAL_SECRET || 'clave-segura',
+
+module.exports = {
+    flowFile: process.env.FLOWS_FILE || 'flows.json',
+    credentialSecret: process.env.CREDENTIAL_SECRET || 'clave-segura',
+
+    httpAdminRoot: "/",
+    httpNodeRoot: "/",
+    userDir: "./.nodered",
+
+    functionGlobalContext: {},
+
+    editorTheme: {
+        projects: {
+            enabled: false
+        }
+    }
+}
